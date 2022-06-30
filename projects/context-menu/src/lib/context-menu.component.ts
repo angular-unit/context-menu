e9714012a7d8229ca50cd22309cd784cf938f18f
Meile <alan.meile@zuehlke.com>
2022-06-30 19:37:48 +0200

    initial commit

---
+++ projects/context-menu/src/lib/context-menu.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-context-menu',
+  template: `
+    <p>
+      context-menu works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ContextMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/context-menu/src/lib/context-menu.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { ContextMenuComponent } from './context-menu.component';
+
+
+
+@NgModule({
+  declarations: [
+    ContextMenuComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ContextMenuComponent
+  ]
+})
+export class ContextMenuModule { }
